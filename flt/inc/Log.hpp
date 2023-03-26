477ea317e179cc1e6de148e623f23ee862f4c75b
Miguel Gueifao <miguel.gueifao@gmail.com>
2023-03-26 13:53:05 +0200

    Changed macro names to reduce clashing possibility

--- flt/inc/Log.hpp
+++ flt/inc/Log.hpp
@@ -8,20 +8,20 @@
 namespace flt
 {
 
-#define TRACE 0
-#define DEBUG 1
-#define INFO 2
-#define WARN 3
-#define ERROR 4
-#define FATAL 5
+#define FLT_LOG_TRACE_LVL 0
+#define FLT_LOG_DEBUG_LVL 1
+#define FLT_LOG_INFO_LVL 2
+#define FLT_LOG_WARN_LVL 3
+#define FLT_LOG_ERROR_LVL 4
+#define FLT_LOG_FATAL_LVL 5
 
-#define RESET_COLOR "\033[0m"
-#define TRACE_COLOR_FG "\033[38;5;238m"
-#define DEBUG_COLOR_FG "\033[38;5;243m"
-#define INFO_COLOR_FG "\033[38;5;255m"
-#define WARN_COLOR_FG "\033[38;5;11m"
-#define ERROR_COLOR_FG "\033[38;5;160m"
-#define FATAL_COLOR_FG "\033[38;5;0m\033[48;5;160m"
+#define FLT_LOG_RESET_COLOR "\033[0m"
+#define FLT_LOG_TRACE_COLOR_FG "\033[38;5;238m"
+#define FLT_LOG_DEBUG_COLOR_FG "\033[38;5;243m"
+#define FLT_LOG_INFO_COLOR_FG "\033[38;5;255m"
+#define FLT_LOG_WARN_COLOR_FG "\033[38;5;11m"
+#define FLT_LOG_ERROR_COLOR_FG "\033[38;5;160m"
+#define FLT_LOG_FATAL_COLOR_FG "\033[38;5;0m\033[48;5;160m"
 
 #define LOG_OPEN_FILE(path)
 #define LOG_CLOSE_FILE(path)
@@ -41,35 +41,35 @@ namespace flt
 	#undef LOG_CLOSE_FILE
 	#define LOG_CLOSE_FILE(path) flt::Log::closeFile(path);
 
-	#if (LOG_LVL <= TRACE)
+	#if (LOG_LVL <= FLT_LOG_TRACE_LVL)
 		#undef LOG_TRACE
 		#define LOG_TRACE(msg) {std::stringstream ss; ss<<msg; flt::Log::trace(ss);}
-	#endif // TRACE lvl
+	#endif // FLT_LOG_TRACE_LVL lvl
 
-	#if (LOG_LVL <= DEBUG)
+	#if (LOG_LVL <= FLT_LOG_DEBUG_LVL)
 		#undef LOG_DEBUG
 		#define LOG_DEBUG(msg) {std::stringstream ss; ss<<msg; flt::Log::debug(ss);}
-	#endif // DEBUG lvl
+	#endif // FLT_LOG_DEBUG_LVL lvl
 
-	#if (LOG_LVL <= INFO)
+	#if (LOG_LVL <= FLT_LOG_INFO_LVL)
 		#undef LOG_INFO
 		#define LOG_INFO(msg) {std::stringstream ss; ss<<msg; flt::Log::info(ss);}
-	#endif // INFO lvl
+	#endif // FLT_LOG_INFO_LVL lvl
 
-	#if (LOG_LVL <= WARN)
+	#if (LOG_LVL <= FLT_LOG_WARN_LVL)
 		#undef LOG_WARN
 		#define LOG_WARN(msg) {std::stringstream ss; ss<<msg; flt::Log::warn(ss);}
-	#endif // WARN lvl
+	#endif // FLT_LOG_WARN_LVL lvl
 
-	#if (LOG_LVL <= ERROR)
+	#if (LOG_LVL <= FLT_LOG_ERROR_LVL)
 		#undef LOG_ERROR
 		#define LOG_ERROR(msg) {std::stringstream ss; ss<<msg; flt::Log::error(ss);}
-	#endif // ERROR lvl
+	#endif // FLT_LOG_ERROR_LVL lvl
 
-	#if (LOG_LVL <= FATAL)
+	#if (LOG_LVL <= FLT_LOG_FATAL_LVL)
 		#undef LOG_FATAL
 		#define LOG_FATAL(msg) {std::stringstream ss; ss<<msg; flt::Log::fatal(ss);}
-	#endif // FATAL lvl
+	#endif // FLT_LOG_FATAL_LVL lvl
 
 #endif // LOG_LVL
 
@@ -88,6 +88,13 @@ public:
 	static void error(const std::stringstream& msg);
 	static void fatal(const std::stringstream& msg);
 };
+
+#undef FLT_LOG_TRACE_LVL
+#undef FLT_LOG_DEBUG_LVL
+#undef FLT_LOG_INFO_LVL
+#undef FLT_LOG_WARN_LVL
+#undef FLT_LOG_ERROR_LVL
+#undef FLT_LOG_FATAL_LVL
 
 } // namespace flt
 
--- flt/src/log/Log.cpp
+++ flt/src/log/Log.cpp
@@ -25,8 +25,8 @@ flt::Log::closeFile(void) {
 
 void
 flt::Log::trace(const std::stringstream& msg) {
-	std::clog << TRACE_COLOR_FG << "[TRACE]" << RESET_COLOR << ": " <<
-		msg.str() << RESET_COLOR << std::endl;
+	std::clog << FLT_LOG_TRACE_COLOR_FG << "[TRACE]" << FLT_LOG_RESET_COLOR << ": " <<
+		msg.str() << FLT_LOG_RESET_COLOR << std::endl;
 	if (flt::Log::fout.is_open()) {
 		flt::Log::fout << "[TRACE]: " << msg.str() << std::endl;
 		flt::Log::fout.flush();
@@ -36,8 +36,8 @@ flt::Log::trace(const std::stringstream& msg) {
 
 void
 flt::Log::debug(const std::stringstream& msg) {
-	std::clog << DEBUG_COLOR_FG << "[DEBUG]" << RESET_COLOR << ": " <<
-		msg.str()  << RESET_COLOR << std::endl;
+	std::clog << FLT_LOG_DEBUG_COLOR_FG << "[DEBUG]" << FLT_LOG_RESET_COLOR << ": " <<
+		msg.str()  << FLT_LOG_RESET_COLOR << std::endl;
 	if (flt::Log::fout.is_open()) {
 		flt::Log::fout << "[DEBUG]: " << msg.str() << std::endl;
 		flt::Log::fout.flush();
@@ -46,8 +46,8 @@ flt::Log::debug(const std::stringstream& msg) {
 
 void
 flt::Log::info(const std::stringstream& msg) {
-	std::clog << INFO_COLOR_FG << "[INFO]" << RESET_COLOR << ": " <<
-		msg.str()  << RESET_COLOR << std::endl;
+	std::clog << FLT_LOG_INFO_COLOR_FG << "[INFO]" << FLT_LOG_RESET_COLOR << ": " <<
+		msg.str()  << FLT_LOG_RESET_COLOR << std::endl;
 	if (flt::Log::fout.is_open()) {
 		flt::Log::fout << "[INFO]: " << msg.str() << std::endl;
 		flt::Log::fout.flush();
@@ -56,8 +56,8 @@ flt::Log::info(const std::stringstream& msg) {
 
 void
 flt::Log::warn(const std::stringstream& msg) {
-	std::clog << WARN_COLOR_FG << "[WARN]" << RESET_COLOR << ": " <<
-		msg.str()  << RESET_COLOR << std::endl;
+	std::clog << FLT_LOG_WARN_COLOR_FG << "[WARN]" << FLT_LOG_RESET_COLOR << ": " <<
+		msg.str()  << FLT_LOG_RESET_COLOR << std::endl;
 	if (flt::Log::fout.is_open()) {
 		flt::Log::fout << "[WARN]: " << msg.str() << std::endl;
 		flt::Log::fout.flush();
@@ -66,8 +66,8 @@ flt::Log::warn(const std::stringstream& msg) {
 
 void
 flt::Log::error(const std::stringstream& msg) {
-	std::clog << ERROR_COLOR_FG << "[ERROR]" << RESET_COLOR << ": " <<
-		msg.str()  << RESET_COLOR << std::endl;
+	std::clog << FLT_LOG_ERROR_COLOR_FG << "[ERROR]" << FLT_LOG_RESET_COLOR << ": " <<
+		msg.str()  << FLT_LOG_RESET_COLOR << std::endl;
 	if (flt::Log::fout.is_open()) {
 		flt::Log::fout << "[ERROR]: " << msg.str() << std::endl;
 		flt::Log::fout.flush();
@@ -76,8 +76,8 @@ flt::Log::error(const std::stringstream& msg) {
 
 void
 flt::Log::fatal(const std::stringstream& msg) {
-	std::clog << FATAL_COLOR_FG << "[FATAL]" << RESET_COLOR << ": " <<
-		msg.str()  << RESET_COLOR << std::endl;
+	std::clog << FLT_LOG_FATAL_COLOR_FG << "[FATAL]" << FLT_LOG_RESET_COLOR << ": " <<
+		msg.str()  << FLT_LOG_RESET_COLOR << std::endl;
 	if (flt::Log::fout.is_open()) {
 		flt::Log::fout << "[FATAL]: " << msg.str() << std::endl;
 		flt::Log::fout.flush();
